ff9dd0875a58b288324450a8bc0cb049fbb28360
gaurav20200 <gauravbadhani86@gmail.com>
2021-09-26 23:58:50 +0530

    11. Container With Most Water

--- Day7/31.cpp
+++ Day7/31.cpp
@@ -2,10 +2,12 @@ class Solution {
 public:
     int maxArea(vector<int>& height) {
         int ans=0;
-        for(int i=0;i+1<height.size();i++){
-            for(int  j=i+1;j<height.size();j++){
-                ans=max(ans,(j-i)*min(height[i],height[j]));
-            }
+       int i=0,j=height.size()-1;
+        while(i<j){
+            ans=max(ans,(j-i)*min(height[i],height[j]));
+            if(height[i]<height[j]){
+                i++;
+            }else j--;
         }
         return ans;
     }
