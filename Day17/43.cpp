f507aff2337e65ac3bd1625b7ec9276f18f3c899
unknown <gauravbadhani86@gmail.com>
2021-10-09 22:43:13 +0530

    695. Max Area of Island

--- Day17/43.cpp
+++ Day17/43.cpp
@@ -66,3 +66,52 @@ int main(){
  cout<<s.maxAreaOfIsland(grid)<<'\n';
 return 0;
 }
+
+
+
+
+
+//BEST APPROACH USING BFS
+
+class Solution {
+public:
+    int valid(int x,int y,int n,int m){
+        if(x<0||x>=n)return 0;
+        if(y<0||y>=m)return 0;
+        return 1;
+    }
+    int dirx[4]={0,0,1,-1};
+    int diry[4]={-1,1,0,0};
+    int ans=0;
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+       queue<pair<int,int>>q;
+        int n=grid.size();
+        int m=grid[0].size();
+        for(int i=0;i<n;i++){
+         for(int j=0;j<m;j++){
+             int cur=0;
+             if(grid[i][j]==1){
+                int curr=1;
+                q.push({i,j});
+                 grid[i][j]=0;
+                 while(!q.empty()){
+                     auto cur=q.front();
+                 for(int k=0;k<4;k++){
+                     int x=cur.first+dirx[k];
+                     int y=cur.second+diry[k];
+                     if(valid(x,y,n,m)&&grid[x][y]==1){
+                         q.push({x,y});
+                         curr++;
+                         grid[x][y]=0;
+                     }
+                 }
+                     
+            q.pop();
+            }
+                 ans=max(ans,curr);
+        }
+         }
+        }
+        return ans;
+    }
+};
